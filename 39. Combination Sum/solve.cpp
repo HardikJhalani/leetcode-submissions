f2fb0d88edfc65cdcebfedcbfc01d58f68ee9735
Hardik Jhalani <hardikjhalani23092001@gmail.com>
2022-07-10 00:54:25 +0530

    added combination sum

---
+++ 39. Combination Sum/solve.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    
+    void findCombination(int i, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds) {
+        if(i == arr.size()) {
+            if(target == 0) {
+                ans.push_back(ds);
+            }
+            return;
+        }
+
+        if(target >= arr[i]) {
+            ds.push_back(arr[i]);
+            findCombination(i, target - arr[i], arr, ans, ds);
+            ds.pop_back();
+        }
+        findCombination(i+1, target, arr, ans, ds);
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        findCombination(0, target,  candidates, ans, ds);
+        return ans;
+    }
+};
